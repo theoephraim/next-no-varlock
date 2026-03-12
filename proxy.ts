f32ce802fecf7765a722dbc4ad871d38ba752e8c
Theo Ephraim <theo@dmno.dev>
2026-03-12 12:32:17 -0700

    add NEXT_PUBLIC_ var to test

--- proxy.ts
+++ proxy.ts
@@ -2,6 +2,7 @@ import type { NextRequest } from 'next/server';
 
 export function proxy(_request: NextRequest) {
   console.log('proxy env vars', {
+    NEXT_PUBLIC_VAR_FROM_ENV: process.env.NEXT_PUBLIC_VAR_FROM_ENV,
     VAR_FROM_ENV: process.env.VAR_FROM_ENV,
     VAR_FROM_UI: process.env.VAR_FROM_UI,
   });
